a838e7998384bf119efe6fd106d17e2bde1ee7f0
Peter Fasula <17052780+programminPete@users.noreply.github.com>
2023-10-26 14:43:56 -0400

    Update AuthLoadingScreen.tsx

--- frontend/dogWalkMobile/src/screens/AuthLoadingScreen.tsx
+++ frontend/dogWalkMobile/src/screens/AuthLoadingScreen.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-/**
- * @todo - note - there is react-native-splash-screen that I thought should be
- * created at launch , but doesn't seem to be working properly, so using this
- * Need to either Remove that package, if this works ,or set that up to be the launch screen
- * @returns
- */
 export default function AuthLoadingScreen() {
   return (
     <View style={styles.container}>
